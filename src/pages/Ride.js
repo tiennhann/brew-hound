17ac7f5a7455776b6fceb3530d7c59fb87f5d736
Navarre Brown <skeeter9@nmsu.edu>
2023-04-19 15:36:50 -0600

    request a ride update

--- src/pages/Ride.js
+++ src/pages/Ride.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "../styles/Ride.css";
+import logo from '../assets/img/ride2.png'
 
 function Ride() {
   return (
-    <div className="ride" >
-    <h1>Ride</h1>
+    <div>
+      <img src={logo} alt='Ride logo'/>
+      <h1>Too Drunk To Drive?</h1>
+      <h3>Crimson Cab</h3>
+      <p>
+          Hours of Operation: Every Day -- 6:00 P.M. - 2:00 A.M.
+          <br></br>
+          Call 575-524-8294!
+      </p>
     </div>
   )
 }
