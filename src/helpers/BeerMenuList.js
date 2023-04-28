7f843ed5aaefd9409236ce3f953932a297689998
Ne'kko Montoya <87720386+nekMont@users.noreply.github.com>
2023-04-27 19:23:01 -0600

    aromas and mouthfeel contents added

--- src/helpers/BeerMenuList.js
+++ src/helpers/BeerMenuList.js
@@ -29,119 +29,119 @@ export const BeerMenuList = [
         image: AmberLager,
         ABV: "4.5-6.0%",
         description: ": A widely available, sessionable craft beer style that showcases both malt and hops. Amber lagers are a medium-bodied lager with a toasty or caramel-like malt character. Hop bitterness can range from very low to medium-high. Brewers may use decoction mash and dry-hopping to achieve advanced flavors.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Sweet and Malty",
+        Mouthfeel:"Moderate carbination, medium-full body, and alcohol warm."
     },
     {
         name: "American Barley Wine",
         image: AmericanBarleyWine,
         ABV: "6.0-12.0%",
         description:": Similar malt and hop flavors as the aroma (same descriptors apply). Moderately strong to aggressive bitterness,tempered by a rich, malty palate. Moderate to high hop flavor. Low to moderate esters. Noticeable alcohol, but not solventy. Moderately low to moderately high malty sweetness on the palate, with a somewhat malty to dry but full finish. Age will often dry out the beer, and smooth out the flavors. The balanceis malty, but always bitter. ",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Hops, rich malt, Caramel sweetness.",
+        Mouthfeel:"Moderate carbination, medium full bodied."
     },
     {
         name: "American Black Ale",
         image: AmericanBlackAle,
         ABV: "6.0-9.0%",
         description:": The American black ale is characterized by the perception of caramel malt and dark roasted malt flavor and aroma. Hop bitterness is perceived to be medium-high to high. Hop flavor and aroma are medium-high. Fruity, citrus, piney, floral and herbal character from hops of all origins may contribute to the overall experience. This beer is often called a black IPA or Cascadian dark ale.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Hops, Citrus, Pine, and floral notes. Roasted malt aroma, caramel",
+        Mouthfeel:"Creamy smooth, Bitter, medium bodied."
     },
     {
         name: "American Brett",
         image: AmericanBrett,
         ABV: "4.5-6.0%",
         description:": Horsey, goaty, leathery, phenolic and some fruity acidic character derived from Brettanomyces organisms may be evident, but in balance with other components of an American Brett beer.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Tropical, Floral, sour, or tart aroma",
+        Mouthfeel:"Moderate to high carbonation level, Finish is dry, with earthy after tastes."
     },
     {
         name: "American Brown Ale",
         image: AmericanBrownAle,
         ABV: "4.2-5.9%",
         description:": Medium to moderately-high malty-sweet or maltyrich flavor with chocolate, caramel, nutty, or toasty malt complexity, with medium to medium-high bitterness. Medium to medium-dry finish with an aftertaste of both malt and hops. Light to moderate hop flavor, sometimes citrusy, fruity, or tropical, although any hop flavor that complements the malt is acceptable. Very low to moderate fruity esters. The malt and hops are generally equal in intensity, but the balance can vary in either direction. Should not have a roasted character suggestive of a Porter or Stout",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"malt forware aroma, with notes of caramel, toffeee, and nutty aromas being common.",
+        Mouthfeel:"Medium bodied, with high carbination level,  slightly nutty and toasty."
     },
     {
         name: "American Cream Ale",
         image: AmericanCreamAle,
         ABV: "4.2-5.6%",
         description:": ell-attenuated; Low to medium malty sweetness; Hoppy bitterness will be low; Well-balanced hop flavors with spicy, floral or herbal notes; Low amounts of diacetyl; Fruity esters possible; Dry & Sweet finish.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"subtle malt aroma, with hints of grain, corn.",
+        Mouthfeel:"Light-bodied, with carbonation level, beer may have a slightly sweet flavor."
     },
     {
         name: "American Imperial Porter",
         image: AmericanImperialPorter,
         ABV: "~9.3%",
         description:": The American-style imperial stout is the strongest in alcohol and body of the stouts. Black in color, these beers typically have an extremely rich malty flavor and aroma with full, sweet malt character. Bitterness can come from roasted malts or hop additions",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Rich complex aroma, with strong malt character",
+        Mouthfeel:"Full-bodied, with low carbination."
     },
     {
         name: "American Imperial Red Ale",
         image: AmericanImperialRedAle,
         ABV: "4.5-7.0%",
         description:": The use of American hops in the American imperial red ale lends to the perception of medium hop bitterness, flavor and aroma. Coupled with a solid malt profile, this should be a beer with balance between hop bitterness and malt sweetness. This is another example of modern American brewers taking an established style and boosting the flavor. California brewers are credited with creating this innovative style.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Strong hop aroma, malt with hidden notes of caramel.",
+        Mouthfeel:"Full bodied with moderate to high carbonation level."
     },
     {
         name: "American Imperial Stout",
         image: AmericanImperialStout,
         ABV: "7.5-13%",
         description:"Intense, complex & rich; Variable ranges of roastiness, malt tones, hoppiness & fruity esters. Notes of chocolate, coffee, burnt grain & dark fruits are common",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Prominent roasted malt character",
+        Mouthfeel:"Full bodied, with low to moderate carbonation."
     },
     {
         name: "American India Pale Ale/IPA",
         image: IndiaPaleAle,
         ABV: "5.0-7.5%",
         description:"loral, fruity, citrus-like, piney or resinous American-variety hop character, the IPA beer style is all about hop flavor, aroma and bitterness.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Strong hop aroma, with notes of citrus, pine, and floral aromas.",
+        Mouthfeel:"medium bodied, with moderate to high carbonation level."
     },
     {
         name: "American Pale Ale",
         image: AmericanPaleAle,
         ABV: "3.2-3.8%",
         description:": Hop and malt character similar to aroma (same intensities and descriptors apply). Caramel flavors are often absent or fairly restrained, but are acceptable as long as they don’t clash with the hops. Moderate to high bitterness. Clean fermentation profile. Fruity yeast esters can be moderate to none, although many hop varieties are quite fruity. Medium to dry finish. The balance is typically towards the late hops and bitterness; the malt presence should be supportive, not distracting. Hop flavor and bitterness often linger into the finish, but the aftertaste should generally be clean and not harsh. Fresh dry-hop flavor optional.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Moderate hop aroma, with notes of citrus and pine",
+        Mouthfeel:"Medium bodied, with moderate to high carbonation level"
     },
     {
         name: "American Sour",
         image: AmericanSour,
         ABV: "~6.0%",
         description:": The profiles of the resulting brews vary wildly, but most of them have a tart, acidic flavor, like biting into raspberries soaked in champagne. American sours fall under the sour ale umbrella, with the differentiating factor being that they're made in America.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"tart aroma, with notes of citrus",
+        Mouthfeel:"typically light to medium bodied with low to moderate carbonation."
     },
     {
         name: "American Stout",
         image: AmericanStout,
         ABV: "5.5-8.5%",
         description:": Moderate to very high roasted flavors, often tasting of coffee, dark or bittersweet chocolate, or roasted coffee beans. May taste of slightly burnt coffee grounds, but this character should not be prominent. Low to medium malt sweetness, often with rich chocolate or caramel flavors. Medium to high bitterness. Low to high hop flavor, generally citrusy or resiny. Medium to dry finish, occasionally with a lightly burnt quality. Low esters optional. Light but smooth alcohol flavor optional.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Strong roasted malt aroma, notes of chocolate, coffee",
+        Mouthfeel:"Full-bodied with a moderate to high carbonation level"
     },
     {
         name: "American Style Wheat Wine Ale",
         image: AmericanStyleWheatWineAle,
         ABV: "8.5-12.2%",
         description:": bready, doughy, or cracker-like, which can extend into the finish. Hop bitterness should be higher than a German Wheat but remain modest, in the range of low to medium. The bitterness can linger into a somewhat crisp finish or disappear behind a slightly sweeter end.",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"Has moderate to strong malt aroma, with notes of bread, biscuit, or caramel.",
+        Mouthfeel:"Full bodied with moderate to low carbonation."
     },
     {
         name: "American Wheat",
         image: AmericanWheat,
         ABV: "4.0-5.5%",
         description:": Light to moderately-strong bready, doughy, or grainy wheat flavor, which can linger into the finish. May have a moderate malty sweetness or can finish quite dry and crisp.Low to moderate hop bitterness, sometimes lasting into the finish. Balance is usually even, but may be slightly bitter. Low to moderate citrusy, spicy, floral, or fruity hop flavor. Moderate esters optional. No banana. No clove phenols. ",
-        Aroma:"",
-        Mouthfeel:""
+        Aroma:"American wheat beer typically has a moderate to low malt aroma, with notes of bread, biscuit, and or cereal.",
+        Mouthfeel:"typically light to medium bodied with high carbonation."
     },
 ];
