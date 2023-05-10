7c14c108281297e45b98539082013bf336e21e03
Navarre Brown <skeeter9@nmsu.edu>
2023-05-10 09:02:04 -0600

    removed old modal code

--- src/components/BeerItem.js
+++ src/components/BeerItem.js
@@ -29,29 +29,7 @@ function BeerItem({image, name, ABV, description, index}) {
                     </Card.Body>
                 </Card>
             </Link>
-        </div>
-
-        <Modal show={show} onHide={handleClose} size='xl' aria-labelledby='contained-modal-title-vcenter' centered>
-            <Modal.Body>
-                <Container id='contained-modal-title-vcenter'>
-                    <Row className='align-items-center'>
-                        <Col md='auto'>
-                            
-                        </Col>
-                        <Col md='auto'>
-                            <h3>{name}</h3>
-                            <img className='resize' src={image}/>
-                        </Col>
-                        <Col style={{  }}> 
-                            
-                            <p> {description} </p>
-                        </Col>
-                    </Row>
-                </Container>
-            </Modal.Body>
-        </Modal>
-
-            
+        </div> 
         </>
     );
 }
